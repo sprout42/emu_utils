0c51585560981972e482e8ab021da401303da47a
Erin Cornelius <a.aaron.cornelius@gmail.com>
2022-07-26 13:56:34 -0400

    made floatstuff utility more useful

--- floatstuff.py
+++ floatstuff.py
@@ -2,21 +2,35 @@ import struct
 
 
 def single_info(val):
-  iee1754_val = struct.unpack('>I', struct.pack('>f', val))[0]
-  exp = (iee1754_val & 0x7F80_0000) >> 23
-  frac = iee1754_val & 0x007F_FFFF
-  result = (2**(exp-127)) * (1+(frac/(2**23)))
+    if isinstance(val, int):
+        iee1754_val = val
+        exp = (iee1754_val & 0x7F80_0000) >> 23
+        frac = iee1754_val & 0x007F_FFFF
+        result = struct.unpack('>f', struct.pack('>I', iee1754_val))[0]
 
-  print('Single Precision:')
-  print('%08x: %03x %05x' % (iee1754_val, exp, frac))
-  print('%.60f' % result)
+    else:
+        iee1754_val = struct.unpack('>I', struct.pack('>f', val))[0]
+        exp = (iee1754_val & 0x7F80_0000) >> 23
+        frac = iee1754_val & 0x007F_FFFF
+        result = (2**(exp-127)) * (1+(frac/(2**23)))
+
+    print('Single Precision:')
+    print('%08x: %03x %05x' % (iee1754_val, exp, frac))
+    print('%.60f' % result)
 
 def double_info(val):
-  iee1754_val = struct.unpack('>Q', struct.pack('>d', val))[0]
-  exp = (iee1754_val & 0x7FF0_0000_0000_0000) >> 52
-  frac = iee1754_val & 0x00F_FFFF_FFFF_FFFF
-  result = (2**(exp-1023)) * (1+(frac/(2**52)))
+    if isinstance(val, int):
+        iee1754_val = val
+        exp = (iee1754_val & 0x7FF0_0000_0000_0000) >> 52
+        frac = iee1754_val & 0x00F_FFFF_FFFF_FFFF
+        result = struct.unpack('>d', struct.pack('>Q', iee1754_val))[0]
 
-  print('Double Precision:')
-  print('%08x: %03x %05x' % (iee1754_val, exp, frac))
-  print('%.60f' % result)
+    else:
+        iee1754_val = struct.unpack('>Q', struct.pack('>d', val))[0]
+        exp = (iee1754_val & 0x7FF0_0000_0000_0000) >> 52
+        frac = iee1754_val & 0x00F_FFFF_FFFF_FFFF
+        result = (2**(exp-1023)) * (1+(frac/(2**52)))
+
+    print('Double Precision:')
+    print('%016x: %03x %05x' % (iee1754_val, exp, frac))
+    print('%.60f' % result)
